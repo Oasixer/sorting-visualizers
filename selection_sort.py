e1dc4e9fafe0f7e0e48ae06c3b8afc944f62f7b0
yoga <kaelan.ms@gmail.com>
2019-05-17 08:20:29 -0400

    added documentation, cleaned up variable names

--- selection_sort.py
+++ selection_sort.py
@@ -3,33 +3,38 @@ import numpy as np
 from matplotlib import cm
 import math
 import tqdm
+'''
+Selection sort visualizer by Kaelan Moffett-Steinke.
+Generates 'selection_sort.avi' using openCV's VideoWriter.
+Inspired by these gifs
+https://imgur.com/gallery/voutF
+'''
 
+# width and height are for the matrix of pixels that gets sorted and by extension the video resolution
 width = 200
 height = 200
 writer = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 60, (width, height))
-# might need to be
 mat = np.zeros((height, width))
-# (rows, cols, channels)
 one_row = np.linspace(0, 255, num=width)
 
 for row in range(height):
     np.random.shuffle(one_row)
     mat[row] = one_row.copy()
 
-#mat = np.array(np.random.shuffle(_row.copy().)
-
-# below might need to be height, width
 mat_hsv = np.random.randint(0, 1, (height, width, 3))
 
-for work in range(mat.shape[0]):
-    for fuck in range(mat.shape[1]):
-        not_rgb = cm.viridis(int(mat[work, fuck]))
-        mat_hsv[work, fuck, 0] = int(math.ceil(not_rgb[0] * 255))
-        mat_hsv[work, fuck, 1] = int(math.ceil(not_rgb[1] * 255))
-        mat_hsv[work, fuck, 2] = int(math.ceil(not_rgb[2] * 255))
+# Note:
+# mat.shape()[0] == nRows, mat.shape()[1] == nCols
+for row in range(mat.shape[0]):
+    for col in range(mat.shape[1]):
+        not_rgb = cm.viridis(int(mat[row, col]))
+        mat_hsv[row, col, 0] = int(math.ceil(not_rgb[0] * 255))
+        mat_hsv[row, col, 1] = int(math.ceil(not_rgb[1] * 255))
+        mat_hsv[row, col, 2] = int(math.ceil(not_rgb[2] * 255))
 
-# numpything.shape()[0] == rows
-num_samples = mat.shape[1]
+# Note:
+# mat.shape()[0] == nRows, mat.shape()[1] == nCols
+num_samples = mat.shape[1]  # Num samples for ETA
 t = tqdm.tqdm(
     total=num_samples,
     dynamic_ncols=True,
@@ -37,7 +42,7 @@ t = tqdm.tqdm(
     bar_format=
     '{l_bar}{bar}|[Elapsed: {elapsed}][Remaining: {remaining}][Samples: {n_fmt}/{total_fmt}][{rate_fmt}{postfix}]'
 )
-#for retarded in range(mat.shape[1]):
+
 for i in range(mat.shape[1]):
     # Write frame
     if (i % 1 == 0):
@@ -54,20 +59,12 @@ for i in range(mat.shape[1]):
         min_j = j
         min_k = i
         for k in range(i, mat.shape[1]):
-            #k = i + 1
             if mat[j, k] < minim:
-                #mat[j, i] = mat[j, k]
-                #mat[j, k] = base
-                #mat_hsv[j, i] = mat_hsv[j, k].copy()
-                #mat_hsv[j, k] = base_hsv
-
                 minim = mat[j, k].copy()
                 minim_hsv = mat_hsv[j, k].copy()
                 min_j = j
                 min_k = k
-        # for loop ends here
         mat[j, i] = minim
-        #print(minim_hsv)
         mat_hsv[j, i] = minim_hsv
         mat[min_j, min_k] = base
         mat_hsv[min_j, min_k] = base_hsv
